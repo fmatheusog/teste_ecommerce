2cd48519557ac136ba7e781915940c329542fe8b
Felipe Matheus Mendes Mori <fmatheusog@gmail.com>
2024-12-27 17:37:52 -0300

    fix: incomplete order model

--- Teste.Shared/Models/OrderModel.cs
+++ Teste.Shared/Models/OrderModel.cs
@@ -9,8 +9,20 @@ public sealed class OrderModel
     public Guid OrderId { get; set; }
     [JsonPropertyName("dataVenda")]
     public DateTime OrderDate { get; set; }
-    [JsonPropertyName("cpf")]
+    [JsonPropertyName("cliente")]
     public CustomerModel Customer { get; set; } = default!;
-    [JsonPropertyName("categoria")]
+    [JsonPropertyName("itens")]
     public IEnumerable<OrderItemModel> OrderItems { get; set; } = default!;
+    [JsonPropertyName("subtotal")]
+    public decimal SubTotal { get; set; }
+
+    [JsonPropertyName("desconto")]
+    public decimal Discounts { get; set; }
+
+    [JsonPropertyName("valorTotal")]
+    public decimal Total { get; set; }
+
+    [JsonPropertyName("status")]
+    public OrderStatus Status { get; set; }
+
 }
