378d1dadb5f83d486e897ca72a128a244c8b634c
Felipe Matheus Mendes Mori <fmatheusog@gmail.com>
2024-12-28 19:43:01 -0300

    feat: add ui components

--- Teste.Web/src/App.tsx
+++ Teste.Web/src/App.tsx
@@ -1,12 +1,7 @@
 import { AppRoutes } from "./routes";
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <ChakraProvider value={defaultSystem}>
-      <AppRoutes />
-    </ChakraProvider>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
